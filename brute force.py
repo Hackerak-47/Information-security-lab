7210af403090df682197ece5ec4b21ebe54a6955
Aastha Khandelwal <khandelwalaastha44@gmail.com>
2026-09-06 11:09:41 +0530

    Update brute force.py

--- brute force.py
+++ brute force.py
@@ -4,7 +4,7 @@ for i in range(26):
     ans = ""
     for ch in text:
         if ch.isalpha():
-            ans += chr((ord(ch.lower()) - 97 - i) % 26 + 97)
+            ans += chr((ord(ch.lower()) - 65 - i) % 26 + 65)
         else:
             ans += ch
     print(i, ans)
