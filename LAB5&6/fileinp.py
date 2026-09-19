ae7305e224df8cd8e5f0877b88aefb4d21a722ff
Aastha Khandelwal <khandelwalaastha44@gmail.com>
2026-09-19 18:19:47 +0530

    Create fileinp.py

---
+++ LAB5&6/fileinp.py
@@ -0,0 +1,16 @@
+name = input("Enter patient name: ")
+age = input("Enter age: ")
+diagnosis = input("Enter diagnosis: ")
+
+with open("patient.txt", "w") as file:
+    file.write("Patient Name: " + name + "\n")
+    file.write("Age: " + age + "\n")
+    file.write("Diagnosis: " + diagnosis + "\n")
+
+// multiple
+with open("patient.txt", "w") as file:
+    file.write("""Patient Name: Aastha
+Age: 20
+Diagnosis: Fever
+Doctor: Dr. Sharma
+""")
