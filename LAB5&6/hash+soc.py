c003083684481148bec1e029bda5bbf5bce42196
Aastha Khandelwal <khandelwalaastha44@gmail.com>
2026-09-19 17:30:42 +0530

    Update hash+soc.py

--- LAB5&6/hash+soc.py
+++ LAB5&6/hash+soc.py
@@ -1,3 +1,4 @@
+//server side
 import socket
 
 def my_hash(message):
@@ -32,5 +33,43 @@ def start_server():
     conn.close()
     server.close()
 
+//client side
+import socket
 
+def my_hash(message):
+    hash_value = 5381
+
+    for ch in message:
+        hash_value = hash_value * 33 + ord(ch)
+        hash_value = hash_value ^ (hash_value >> 16)
+        hash_value = hash_value & 0xFFFFFFFF
+
+    return hash_value
+
+
+def start_client():
+    client = socket.socket()
+
+    client.connect(("localhost", 9999))
+
+    message = input("Enter message: ")
+
+    client.send(message.encode())
+
+    server_hash = int(client.recv(1024).decode())
+
+    local_hash = my_hash(message)
+
+    print("Server hash:", server_hash)
+    print("Local hash:", local_hash)
+
+    if server_hash == local_hash:
+        print("Data is not corrupted")
+    else:
+        print("Data has been modified")
+
+    client.close()
+
+
+start_client()
 start_server()
