4abbc94c04d4949dea1575795338f5a8634bdcdb
Aastha Khandelwal <khandelwalaastha44@gmail.com>
2026-09-19 12:00:51 +0530

    Update ELgamal.py

--- ELgamal.py
+++ ELgamal.py
@@ -1,28 +1,31 @@
 from Crypto.Util.number import inverse
+from Crypto.Random import random
 
 p = 7919
 g = 2
-x = 2999
+
+# Private key
+x = random.randint(1, p - 2)
 
 # Public key
 h = pow(g, x, p)
 
 message = "HELLO"
 
-# Choose k for demonstration
-k = 123
-
 print("Public Key :", (p, g, h))
 print("Private Key:", x)
 
+# Encryption
 ciphertext = []
 
 for ch in message:
 
+    # Random k for every character
+    k = random.randint(1, p - 2)
+
     m = ord(ch)
 
     c1 = pow(g, k, p)
-
     c2 = (m * pow(h, k, p)) % p
 
     ciphertext.append((c1, c2))
@@ -41,18 +44,9 @@ for c1, c2 in ciphertext:
     plaintext += chr(m)
 
 print("Decrypted:", plaintext)
-from Crypto.Random import random
 
-p = 7919
-g = 2
-
-# Private key
-x = random.randint(1, p - 2)
-
-# Public key
-h = pow(g, x, p)
-
-print("Private Key:", x)
-print("Public Key:", (p, g, h))
-
-
+# Verification
+if plaintext == message:
+    print("ElGamal verification successful!")
+else:
+    print("Verification failed!")
